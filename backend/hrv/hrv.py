8aba0b720cf5ff871c0f7d417840882c41bd66d9
Alberto Ivo <albertoivo@gmail.com>
2020-12-11 15:38:33 -0300

    hrv data

--- backend/hrv/hrv.py
+++ backend/hrv/hrv.py
@@ -9,14 +9,14 @@ hrv_bp = Blueprint('hrv', __name__, url_prefix='/hrv')
 
 @hrv_bp.route('/data')
 def data():
-    df = pd.read_excel('hrv/HRV_teste.xlsx', index_col=0)
+    df = pd.read_excel('backend/hrv/HRV_teste.xlsx', index_col=0)
 
     return df.to_json(date_format='iso', orient='index')
 
 
 @hrv_bp.route('/statistics')
 def statistics():
-    df = pd.read_excel('hrv/HRV_teste.xlsx', index_col=0)
+    df = pd.read_excel('backend/hrv/HRV_teste.xlsx', index_col=0)
 
     return df['Reading'].describe().to_json()
 
@@ -24,7 +24,7 @@ def statistics():
 @hrv_bp.route('/graph')
 def hrv_graph():
     try:
-        graph = pd.read_excel('hrv/HRV_teste.xlsx', index_col=0)
+        graph = pd.read_excel('backend/hrv/HRV_teste.xlsx', index_col=0)
         graph = graph.dropna()
         graph[['Ln rMSSD 7-Day Rolling Average', 'Upper limit',
                'Lower Limit']].plot(figsize=(21, 7), grid=True)
